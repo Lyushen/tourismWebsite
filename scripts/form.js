dd195aa1b8bc6ae520cfddc96db3da5a17b3f44d
Lyushen <l3122114@gmail.com>
2023-12-30 13:51:43 +0000

    VIdeo emb and form validation

--- scripts/form.js
+++ scripts/form.js
@@ -1,78 +1,104 @@
-document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('DOMContentLoaded', function () {
+        const form = document.querySelector('#myForm');
+        const nameInput = document.querySelector('#fullName');
+        const emailInput = document.querySelector('#emailAddress');
+        const successMessage = document.querySelector('#formSuccessMessage');
+    
+        // Using querySelector within the parent .form-control div
+        const nameError = document.querySelector('.form-control:nth-child(1) .field-error');
+        const emailError = document.querySelector('.form-control:nth-child(2) .field-error');
+    
+        if (!nameError)
+        console.log("Name Error. The element is ", nameError);
+        if (!emailError)
+        console.log("Email Error. The element is ", emailError);
+    
+        if (!form || !nameInput || !emailInput || !nameError || !emailError) {
+            console.error("Some form elements or error display elements are not found! JS Validation could not be proceed.");
+            return;
+        }
+    
 
-// Define constants for all of the elements we will be working with
-const form = document.querySelector('#myForm');
-const nameInput = document.querySelector('#fullName');
-const emailInput = document.querySelector('#emailAddress');
-const nameError = document.querySelector('#fullName + .field-error');
-const emailError = document.querySelector('#emailAddress + .field-error');
+        function validateName() {
+            const nameParts = nameInput.value.trim().split(/\s+/); // Split by spaces
+            const isNameValid = nameParts.length >= 2 && nameInput.validity.valid;
+        
+            if (isNameValid) {
+                nameError.textContent = '';
+                nameError.style.display = 'none';
+            } else {
+                nameError.textContent = 'Please enter your Full Name (at least two words)';
+                nameError.style.display = 'block';
+            }
+        }
 
-function validateName() {
-  // Code that will run whenever we want to validate the fullName field
-  if (nameInput.validity.valid) {
-    // Value is valid, so remove any previous error message
-    nameError.textContent = '';
-    } else {
-    // Value is not valid, so display an error message
-    showNameError();
-    }
-}
+    function validateEmail() {
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const isValidEmail = emailPattern.test(emailInput.value);
 
-function showNameError() {
-    // Code to display an error message for the fullName field
-    nameError.textContent = 'Please enter your Full name';
-}
-
-function validateEmail() {
-    // Code that will run whenever we want to validate the email field
-    if (emailInput.validity.valid) {
-        // Value is valid, so remove any previous error message
-        emailError.textContent = '';
+        if (isValidEmail) {
+            emailError.textContent = '';
+            emailError.style.display = 'none';
         } else {
-        // Value is not valid, so display an error message
-        showEmailError();
+            if (emailInput.validity.valueMissing) {
+                emailError.textContent = 'Please enter your email address';
+            } else {
+                emailError.textContent = 'Please enter a valid email address';
+            }
+            emailError.style.display = 'block';
         }
+    }
+
+    function isFormValid() {
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const isEmailValid = emailPattern.test(emailInput.value) && emailInput.validity.valid;
+        const isNameValid = nameInput.validity.valid;
+
+        return isEmailValid && isNameValid;
+    }
+
+    function validateForm(event) {
+        validateName();
+        validateEmail();
+
+        if (!isFormValid()) {
+            console.log("Form validation failed");
+            event.preventDefault();
+            if (successMessage) {
+                successMessage.style.display = 'none'; // Hide success message if visible    
+            }
+        } else {
+            console.log("Form validation passed");
+            // Display success message
+            if (successMessage) {
+                successMessage.style.display = 'block';
+            
+            //do our job
+            const formData = {
+                name: nameInput.value,
+                email: emailInput.value
+                // Add other form fields here
+            };
+    
+            fetch('/send-email', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                // Handle success (e.g., show a success message)
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                // Handle error
+            });
         
-}
-
-function showEmailError() {
-    // Code to display an error message for the email field
-    if (emailInput.validity.valueMissing) {
-        // The field is empty
-        emailError.textContent = 'Please enter your email address';
-        } else if (emailInput.validity.typeMismatch) {
-        // The field contains an invalid email address
-        emailError.textContent = 'Your email address does not appear to be correct';
-        }
-        
-}
-
-function validateForm(event) {
-    // Code that we want to run to validate the entire form (both fields)
-    // Keep track of our validation "state"
-let formHasErrors = false;
-if (!nameInput.validity.valid) {
-// Name input is invalid, show its error and change "state"
-formHasErrors = true;
-showNameError();
-}
-if (!emailInput.validity.valid) {
-// Email input is invalid, show its error and change "state"
-formHasErrors = true;
-showEmailError();
-}
-// Check the "state" to see if the form should be submitted
-if (formHasErrors) {
-event.preventDefault();
-}
-}
-
-// Set up an event listener for each of the fields in the form
-// The event we are listening for is "input" (occurs when user types or pastes text into a field)
-nameInput.addEventListener('input', validateName);
-emailInput.addEventListener('input', validateEmail);
-
-// Set up an event listener for the form
-// The event we are listening for is "submit" (occurs when user trys to submit the form)
-form.addEventListener('submit', validateForm);
+        }}}
+    nameInput.addEventListener('input', validateName);
+    emailInput.addEventListener('input', validateEmail);
+    form.addEventListener('submit', validateForm);
 });
